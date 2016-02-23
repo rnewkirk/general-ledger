ee89182d5e4264eec0e9f994c4a618ba039b6b02
Bob Newkirk <rnewkirk@users.noreply.github.com>
2016-02-23 15:51:58 -0500

    Delete default data/clientlist structure. Updated logic to check and create client list file if missing.

--- src/GeneralLedger/ClientMenuController.java
+++ src/GeneralLedger/ClientMenuController.java
@@ -4,10 +4,10 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.Initializable;
 import javafx.scene.control.ListView;
-
-
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -39,13 +39,30 @@ public class ClientMenuController implements Initializable{
 
     public ObservableList<String> getClientList() {
 
-        String fileName = "data/clientList.txt";
+        File file = new File("data/clientList.txt");
+
+        //check file before reading
+        if(!file.isFile()) {
+            File dir = new File("data");
+
+            try{
+                //file missing, check directory before creating file
+                if(!dir.exists()){
+                    dir.mkdir();
+                }
+                FileWriter fw = new FileWriter(file);
+                fw.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
         String line;
         ObservableList<String> clients = FXCollections.observableArrayList();
 
         try {
             FileReader fileReader =
-                    new FileReader(fileName);
+                    new FileReader(file);
 
             BufferedReader bufferedReader =
                     new BufferedReader(fileReader);
@@ -56,8 +73,8 @@ public class ClientMenuController implements Initializable{
 
             bufferedReader.close();
         }
-        catch(IOException ex) {
-            ex.printStackTrace();
+        catch(IOException e) {
+            e.printStackTrace();
         }
 
         FXCollections.sort(clients);
