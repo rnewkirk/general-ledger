0c10c992a442d5eb65ee441e3b891108d11bbdf1
Bob Newkirk <rnewkirk@users.noreply.github.com>
2016-02-24 16:25:03 -0500

    Add H2 embedded database jar file to project and to the library.

--- src/GeneralLedger/inputBox.java
+++ src/GeneralLedger/inputBox.java
@@ -1,5 +1,6 @@
 package GeneralLedger;
 
+import javafx.event.EventHandler;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
@@ -9,6 +10,7 @@ import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
+import javafx.stage.WindowEvent;
 
 public class InputBox {
 
